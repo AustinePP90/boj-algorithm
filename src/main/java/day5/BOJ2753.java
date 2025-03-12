cb64a673e0cc26066fefcb25d01358e8ba5dc49b
Austine <chucky891213@naver.com>
2025-03-12 19:19:49 +0900

    MR: BOJ day5

---
+++ src/main/java/day5/BOJ2753.java
@@ -0,0 +1,16 @@
+package day5;
+
+import java.util.Scanner;
+
+public class BOJ2753 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Y = sc.nextInt();
+
+        if ((Y % 4 == 0 && Y % 100 != 0) || Y % 400 == 0) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
