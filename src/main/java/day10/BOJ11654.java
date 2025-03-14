0ce2ba6062f4f75a3c627ddb7ff32b3e7b2bb96b
Austine <chucky891213@naver.com>
2025-03-14 15:22:02 +0900

    MR: BOJ day10

---
+++ src/main/java/day10/BOJ11654.java
@@ -0,0 +1,15 @@
+package day10;
+
+import java.util.Scanner;
+
+public class BOJ11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        char charWord = word.charAt(0);
+        System.out.println((int)charWord);
+
+//        int num = sc.nextInt();
+//        System.out.println((char)num);
+    }
+}
