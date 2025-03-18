536f5d4d1e45ee9f9814ef7548693f9afb8f3034
Austine <chucky891213@naver.com>
2025-03-18 19:55:34 +0900

    MR: BOJ day19

---
+++ src/main/java/day19/BOJ27866.java
@@ -0,0 +1,13 @@
+package day19;
+
+import java.util.Scanner;
+
+public class BOJ27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int i = sc.nextInt();
+        System.out.println(s.charAt(i-1));
+
+    }
+}
