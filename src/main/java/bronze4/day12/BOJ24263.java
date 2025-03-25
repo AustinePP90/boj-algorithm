ef3c208eec0ff4d94d1b131a28d1e5830d1b7d81
Austine <chucky891213@naver.com>
2025-03-25 15:38:05 +0900

    MR: BOJ b4 day12

---
+++ src/main/java/bronze4/day12/BOJ24263.java
@@ -0,0 +1,13 @@
+package bronze4.day12;
+
+import java.util.Scanner;
+
+public class BOJ24263 {
+    public static void main(String[] args) {
+        // 알고리즘, 빅5? 빅O?
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
