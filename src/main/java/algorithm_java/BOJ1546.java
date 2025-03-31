d6fcfbc0bb8dc5176cfb626b52a4b1d0c5d25ca5
Austine <chucky891213@naver.com>
2025-03-31 22:42:03 +0900

    MR: BOJ1546

--- src/main/java/algorithm_java/BOJ1546.java
+++ src/main/java/algorithm_java/BOJ1546.java
@@ -1,4 +1,34 @@
 package algorithm_java;
 
+import java.util.Scanner;
+
 public class BOJ1546 {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int N = sc.nextInt();
+//        int sum = 0;
+//        int highScore = 0;
+//        for (int i=0; i<N; i++) {
+//            int score = sc.nextInt();
+//            if (score > highScore) highScore = score;
+//            sum += score;
+//        }
+//        System.out.println(sum * 100.0 / highScore / N);
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] score = new int[N];
+        int max = 0;
+        for (int i=0; i<N; i++) {
+            score[i] = sc.nextInt();
+            if (score[i] > max) max = score[i];
+        }
+
+        double sum = 0;
+        for (int i=0; i<N; i++) {
+            sum += (double) score[i] / max * 100;
+        }
+
+        System.out.println(sum / N);
+    }
 }
