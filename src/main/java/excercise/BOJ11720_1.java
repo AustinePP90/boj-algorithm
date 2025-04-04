cb63f0d1a878800d86df20f78c5be4ae1bb6b780
Austine <chucky891213@naver.com>
2025-04-04 20:22:14 +0900

    MR: excercise(BOJ11720, 1546)

---
+++ src/main/java/excercise/BOJ11720_1.java
@@ -0,0 +1,22 @@
+package excercise;
+
+import java.util.Scanner;
+
+public class BOJ11720_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String num = sc.next();
+//        5(  ) <- nextLine() 이용 시 빈칸 입력 받음
+//        54321
+//        String num = sc.nextLine();
+        char[] numArray = num.toCharArray();
+
+        int sum = 0;
+        for (int i=0; i<N; i++) {
+            sum += numArray[i] - '0';
+        }
+
+        System.out.println(sum);
+    }
+}
---
+++ src/main/java/excercise/BOJ1546_1.java
@@ -0,0 +1,24 @@
+package excercise;
+
+import java.util.Scanner;
+
+public class BOJ1546_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] scoreArr = new int[N];
+        int highScore = 0;
+
+        for (int i=0; i<N; i++) {
+            scoreArr[i] = sc.nextInt();
+            if (scoreArr[i] > highScore) highScore = scoreArr[i];
+        }
+
+        double sum = 0;
+        for (int i=0; i<N; i++) {
+            sum += (double) scoreArr[i] / highScore * 100;
+        }
+
+        System.out.println(sum / N);
+    }
+}
