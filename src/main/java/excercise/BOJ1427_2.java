f8deadd7861b9d69c63513599dd87b57951227ed
Austine <chucky891213@naver.com>
2025-04-19 14:51:00 +0900

    MR: excercise(BOJ1427)

---
+++ src/main/java/excercise/BOJ1427_2.java
@@ -0,0 +1,32 @@
+package excercise;
+
+import java.util.Scanner;
+
+public class BOJ1427_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String N = sc.next();
+        int[] NArr = new int[N.length()];
+        for (int i=0; i< NArr.length; i++) {
+            NArr[i] = Integer.parseInt(N.substring(i, i+1));
+        }
+
+        for (int i=0; i< NArr.length; i++) {
+            int Max = i;
+            for (int j=i; j< NArr.length; j++) {
+                if (NArr[j] > NArr[Max]) {
+                    Max = j;
+                }
+            }
+            if (NArr[i] < NArr[Max]) {
+                int temp = NArr[i];
+                NArr[i] = NArr[Max];
+                NArr[Max] = temp;
+            }
+        }
+
+        for (int i=0; i< NArr.length; i++) {
+            System.out.print(NArr[i]);
+        }
+    }
+}
