bd4d54ce44bd3a9a869bbb1d36917c8cf859b6a9
Austine <chucky891213@naver.com>
2025-04-26 15:54:57 +0900

    MR: excercise(BOJ11724, 2178)

---
+++ src/main/java/excercise/BOJ2178_2.java
@@ -0,0 +1,57 @@
+package excercise;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ2178_2 {
+    static int[][] A;
+    static boolean[][] visited;
+    static int N, M;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        A = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String line = st.nextToken();
+            for (int j=0; j<M; j++) {
+                A[i][j] = Integer.parseInt(line.substring(j,j+1));
+            }
+        }
+
+        BFS(0,0);
+        System.out.println(A[N-1][M-1]);
+    }
+
+    public static void BFS(int i, int j) {
+        Queue<int[]> queue = new LinkedList<>();
+
+        queue.offer(new int[] {i,j});
+        visited[i][j] = true;
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            for (int k=0; k<4; k++) {
+                int y = now[0] + dy[k];
+                int x = now[1] + dx[k];
+                if (y >= 0 && x >= 0 && y < N && x < M) {
+                    if (!visited[y][x] && A[y][x] != 0) {
+                        visited[y][x] = true;
+                        A[y][x] = A[now[0]][now[1]] + 1;
+                        queue.offer(new int[] {y ,x});
+                    }
+                }
+            }
+        }
+    }
+}
